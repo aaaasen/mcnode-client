909c5b1861f4b8fcc5ecdf1782440a88bbc32d86
Lane Aasen <laneaasen@gmail.com>
2012-11-02 11:33:04 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+McnodeClient::Application.config.secret_token = '2b1f0bab8e7c5dd115cbfe9b32d8af7091fcb6182d4ec4308ed5d5470a93b747988593f238ba0c34ccb23a631dcaaa60ccb33ca8fd2aac69d5958eda6e45a148'
