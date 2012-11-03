8fa14c5c458672a240317e3f4ac078993d5cdb8a
Lane Aasen <laneaasen@gmail.com>
2012-11-02 22:31:56 -0700

    changed comment's commenter to player for consitency

--- db/migrate/20121103050815_create_comments.rb
+++ db/migrate/20121103050815_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def self.up
     create_table :comments do |t|
       t.string :content
-      t.references :commenter
+      t.references :player
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(:version => 20121103050815) do
 
   create_table "comments", :force => true do |t|
     t.string   "content"
-    t.integer  "commenter_id"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.integer  "player_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "players", :force => true do |t|
