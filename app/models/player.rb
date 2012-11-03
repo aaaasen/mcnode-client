8edadbefaae0e5f52a8c4bd42c57a51b9ade191c
Lane Aasen <laneaasen@gmail.com>
2012-11-02 18:39:32 -0700

    adds basic validation to player

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,3 +1,8 @@
 class Player < ActiveRecord::Base
-  attr_accessible :username, :last_login, :online
+	attr_accessible :username, :last_login, :online
+
+	validates :username, :presence => true,
+						:length => { :minimum => 6, :maximum => 30 },
+						:uniqueness => true
+
 end
