79c2b3c137ebd5e7bd1e6d3b25a1ae093a6d12ce
Lane Aasen <laneaasen@gmail.com>
2012-11-02 18:35:48 -0700

    adds json option, since this will be pretty much an api

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,11 @@
 class PlayersController < ApplicationController
   def index
     @players = Player.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @players }
+    end
   end
 
   def show
